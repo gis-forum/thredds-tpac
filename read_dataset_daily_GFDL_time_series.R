285c8e6423d61a2307023df74db1e13b65befc48
Ivan Hanigan <ivan.hanigan@gmail.com>
2014-06-24 10:51:04 +1000

    Update read_dataset_daily_GFDL_time_series.R

--- read_dataset_daily_GFDL_time_series.R
+++ read_dataset_daily_GFDL_time_series.R
@@ -1,3 +1,4 @@
+# Tom Remenyi (from UTAS) was the person who wrote the R code
 #required the following install function on ubuntu
 install.packages("ncdf", type = "source", configure.args="--with-netcdf-include=/usr/include")
 library(rgdal)
